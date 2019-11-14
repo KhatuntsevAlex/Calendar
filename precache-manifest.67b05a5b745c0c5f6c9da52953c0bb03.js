5c2f744c2013cb0464093c0c437276fab62a1202
Alex <a.khatuntsev2015@gmail.com>
2019-11-14 11:25:39 +0200

    Updates

---
+++ precache-manifest.67b05a5b745c0c5f6c9da52953c0bb03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d22522893950078dbda6c4c69cdd193",
+    "url": "/Calendar/index.html"
+  },
+  {
+    "revision": "e488b8d251e248185bf1",
+    "url": "/Calendar/static/css/2.65048251.chunk.css"
+  },
+  {
+    "revision": "4a4cf74b490700c3b0b6",
+    "url": "/Calendar/static/css/main.94dc49b3.chunk.css"
+  },
+  {
+    "revision": "e488b8d251e248185bf1",
+    "url": "/Calendar/static/js/2.dd5bae7c.chunk.js"
+  },
+  {
+    "revision": "4a4cf74b490700c3b0b6",
+    "url": "/Calendar/static/js/main.30b952db.chunk.js"
+  },
+  {
+    "revision": "8408db116401aba3ccd0",
+    "url": "/Calendar/static/js/runtime-main.857cfa0f.js"
+  }
+]);
